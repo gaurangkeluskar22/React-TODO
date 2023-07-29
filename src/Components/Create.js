57242e820377487e88ce0fb6624407afedef59d7
gaurangkeluskar22 <gaurangkeluskar22@gmail.com>
2023-07-29 20:30:56 +0530

    Changed the UI with MUI

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -2,86 +2,199 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { setDbSearchResultsData, setDbData } from "../Data/Db";
 import { getTodoData } from "../Data/Db";
+import {
+  Card,
+  Box,
+  Stack,
+  Grid,
+  FormControl,
+  TextField,
+  Select,
+  MenuItem,
+  Button,
+  FormHelperText,
+} from "@mui/material";
+import { useForm, Controller } from "react-hook-form";
 
 const Create = () => {
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
   useEffect(() => {
     setTodoData(getTodoData);
   }, []);
 
-  const [todoInputData, setTodoInputData] = useState();
   const [todoData, setTodoData] = useState();
   const navigate = useNavigate();
 
-  const getTodoInputData = (e) => {
-    setTodoInputData({ ...todoInputData, [e.target.name]: e.target.value });
-  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("data after fetching:", todoData);
-    console.log("enterred data:", todoInputData);
+  const onSubmit = (data) => {
+    const todoInputData = {
+      title: data.title,
+      description: data.description,
+      status: data.status,
+    };
+    console.log(data.title, data.desc, data.status);
     todoData.push(todoInputData);
-    console.log("updated tododata:", todoData);
     setDbData(todoData);
     alert("Todo has been created!");
     navigate("/");
   };
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-4"></div>
-        <div className="col-sm-4">
-          <form className=" mx-auto mt-5">
-            <div className="form-group m-1">
-              <label for="exampleInputTitle1" className="">
-                Title
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="todoTitle"
-                placeholder="Enter Title"
-                name="title"
-                onChange={getTodoInputData}
-              />
-            </div>
-            <div className="form-group m-1">
-              <label for="exampleInputDescription1">Description</label>
-              <textarea
-                type="text"
-                className="form-control"
-                id="todoDescription"
-                placeholder="Enter Description"
-                name="description"
-                onChange={getTodoInputData}
-              />
-            </div>
-            <div className="form-group m-1">
-              <label for="exampleInputStatus1">Status</label>
-              <select
-                className="form-select"
-                name="status"
-                onChange={getTodoInputData}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Stack direction="row" justifyContent="center" mt="50px">
+        <Card
+          sx={{
+            width: { sm: "40%", xs: "100%" },
+            display: "flex",
+            justifyContent: "center",
+          }}
+          elevation={10}
+        >
+          <Grid
+            container
+            sx={{ width: { xs: "90%", lg: "60%" } }}
+            pt={5}
+            pb={5}
+          >
+            <Grid item xs={12}>
+              <div>Todo Name:</div>
+              <FormControl
+                fullWidth
+                sx={{
+                  "& .MuiOutlinedInput-notchedOutline": {
+                    borderRadius: "14px",
+                  },
+                }}
               >
-                <option>Select status</option>
-                <option value="pending">Pending</option>
-                <option value="completed">Completed</option>
-              </select>
-            </div>
+                <Controller
+                  name="title"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      fullWidth
+                      placeholder="Enter todo title"
+                      size="small"
+                      required
+                      maxLength={2}
+                      autoComplete="off"
+                      inputProps={{
+                        maxLength: 30,
+                        minLength: 10,
+                      }}
+                      rules={{
+                        required: "Please enter Todo title",
+                        maxLength: {
+                          value: 30,
+                          message: "Maximum character length is 30",
+                        },
+                        minLength: {
+                          value: 10,
+                          message: "Minimum character length is 10",
+                        },
+                        validate: (value) => {
+                          if (value.trim() === "") {
+                            return "Please Enter Title";
+                          }
+                          return true;
+                        },
+                      }}
+                    />
+                  )}
+                />
+              </FormControl>
+              <FormHelperText sx={{ color: "#d32f2f" }}>
+                {errors.title && errors.title.message}
+              </FormHelperText>
+            </Grid>
+            <Grid item xs={12} mt={2}>
+              <div>Todo Description:</div>
 
-            <button
-              type="submit"
-              className="btn btn-primary m-1 w-100"
-              onClick={handleSubmit}
-            >
-              Create
-            </button>
-          </form>
-        </div>
-        <div className="col-sm-4"></div>
-      </div>
-    </div>
+              <FormControl
+                fullWidth
+                sx={{
+                  "& .MuiOutlinedInput-notchedOutline": {
+                    borderRadius: "14px",
+                  },
+                }}
+              >
+                <Controller
+                  name="description"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      fullWidth
+                      placeholder="Enter todo Description"
+                      size="large"
+                      required
+                      multiline
+                      rows={3}
+                      autoComplete="off"
+                      inputProps={{
+                        maxLength: 100,
+                        minLength: 10,
+                      }}
+                      rules={{
+                        required: "Please enter Todo description",
+                        maxLength: {
+                          value: 30,
+                          message: "Maximum character length is 100",
+                        },
+                        minLength: {
+                          value: 10,
+                          message: "Minimum character length is 10",
+                        },
+                        validate: (value) => {
+                          if (value.trim() === "") {
+                            return "Please Enter Description";
+                          }
+                          return true;
+                        },
+                      }}
+                    />
+                  )}
+                />
+              </FormControl>
+              <FormHelperText sx={{ color: "#d32f2f" }}>
+                {errors.description && errors.description.message}
+              </FormHelperText>
+            </Grid>
+            <Grid item xs={12} mt={2}>
+              <div>Todo Status:</div>
+              <FormControl sx={{ width: "100%" }}>
+                <Controller
+                  name="status"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <Select required {...field}>
+                      <MenuItem value="Pending">Pending</MenuItem>
+                      <MenuItem value="Completed">Completed</MenuItem>
+                    </Select>
+                  )}
+                />
+              </FormControl>
+              <FormHelperText sx={{ color: "#d32f2f" }}>
+                {errors.status && errors.status.message}
+              </FormHelperText>
+            </Grid>
+            <Grid item xs={12} mt={2}>
+              <Button variant="contained" type="submit" onClick={handleSubmit}>
+                Create
+              </Button>
+            </Grid>
+          </Grid>
+        </Card>
+      </Stack>
+    </form>
   );
 };
 
